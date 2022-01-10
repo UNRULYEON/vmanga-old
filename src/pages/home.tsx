deddea743b406eb1ea934b4b41483614869070c4
DoritosXL <doritosvslays@gmail.com>
2022-01-10 22:03:55 +0100

    Added skeleton component to the home page. See https://mui.com/components/skeleton/ for more information on usage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,38 @@
 import { auth } from "@fb"
 import { signOut } from "firebase/auth"
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
+
+import "./home.css"
 
 const Home = () => {
   const onSignOut = () => signOut(auth)
 
   return (
-    <div>
+    <div className="home">
       home <button onClick={onSignOut}>sign out</button>
+
+      
+      <div className="homeSkeleton">
+        <div className="homeSkeletonBox">
+          <Stack spacing={1}>                        
+            <Skeleton variant="rectangular" width={150} height={225} />
+            <Skeleton variant="text" width={150} />
+          </Stack>
+        </div>
+        <div className="homeSkeletonBox">
+          <Stack spacing={1}>                        
+            <Skeleton variant="rectangular" width={150} height={225} />
+            <Skeleton variant="text" width={150} />
+          </Stack>
+        </div>
+        <div className="homeSkeletonBox">
+          <Stack spacing={1}>                        
+            <Skeleton variant="rectangular" width={150} height={225} />
+            <Skeleton variant="text" width={150} />
+          </Stack>
+        </div>
+      </div>
     </div>
   )
 }
